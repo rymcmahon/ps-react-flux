09615d1353b83f5fab40c2407c9d15804c9235f7
rymcmahon <toedragdangle9@gmail.com>
2019-11-15 13:17:57 -0500

    update

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,36 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
     getCourses().then(_courses => setCourses(_courses));
-  });
+  }, []);
 
   return (
     <>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author Id</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map(course => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <CourseList courses={courses} />
     </>
   );
 }
