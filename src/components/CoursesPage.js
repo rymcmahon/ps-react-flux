46ca77ecee31dd7221049668e99195f348da0410
rymcmahon <toedragdangle9@gmail.com>
2019-11-19 16:15:32 -0500

    completed everything before flux

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 import CourseList from "./CourseList";
+import { Link } from "react-router-dom";
+import CourseForm from "./CourseForm";
+import PropTypes from "prop-types";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
@@ -12,9 +15,19 @@ function CoursesPage() {
   return (
     <>
       <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add Course
+      </Link>
       <CourseList courses={courses} />
     </>
   );
+}
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 }
 
 export default CoursesPage;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug; // from the path '/courses/:slug'
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course))
+    }
+  }, [props.match.params.slug])
+
+  function handleChange(event) {
+    //Spread operator copies course object
+    setCourse({ ...course, [event.target.name]: event.target.value });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required.";
+    if (!course.authorId) _errors.authorId = "Author is required.";
+    if (!course.category) _errors.category = "Category is required.";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} errors={errors} />
     </>
   );
 };
